4e0c7ba2ee52f854fb252e0bc04642c8af289da7
Aurnytoraink <wiiu3dsxl13127@gmail.com>
2020-07-25 19:55:01 +0200

    First attemp

--- src/tidalgtk/main.py
+++ src/tidalgtk/main.py
@@ -19,8 +19,8 @@ import sys
 import gi
 
 gi.require_version('Gtk', '3.0')
-
-from gi.repository import Gtk, Gio
+gi.require_version('Handy', '1')
+from gi.repository import Gtk, Gio, Handy
 
 from .window import TidalgtkWindow
 
@@ -29,6 +29,9 @@ class Application(Gtk.Application):
     def __init__(self):
         super().__init__(application_id='com.github.Aurnytoraink.TidalGTK',
                          flags=Gio.ApplicationFlags.FLAGS_NONE)
+    def do_startup(self):
+        Gtk.Application.do_startup(self)
+        Handy.init()
 
     def do_activate(self):
         win = self.props.active_window
--- src/tidalgtk/window.py
+++ src/tidalgtk/window.py
@@ -15,14 +15,52 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from gi.repository import Gtk
-
+from gi.repository import Gtk, Handy
+# from tidalgtk.api.session import Session
 
 @Gtk.Template(resource_path='/com/github/Aurnytoraink/TidalGTK/ui/window.ui')
 class TidalgtkWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'TidalgtkWindow'
 
-    label = Gtk.Template.Child()
+    header_stack = Gtk.Template.Child()
+    main_stack = Gtk.Template.Child()
+    app_stack = Gtk.Template.Child()
+    player_reveal = Gtk.Template.Child()
+    switchbar_bottom = Gtk.Template.Child()
+    switchbar_bottom = Gtk.Template.Child()
+    player_timebar = Gtk.Template.Child()
+    play_songinfo = Gtk.Template.Child()
+    enlarge_player_button = Gtk.Template.Child()
+    reduce_player_button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.connect("check-resize",self.update_scale_interface)
+        self.enlarge_player_button.connect("clicked",self.resize_player)
+        self.reduce_player_button.connect("clicked",self.resize_player)
+
+    def update_scale_interface(self, *_):
+        width, height = self.get_size()
+        if width <= 545:
+            self.header_stack.set_visible_child_name("title_header")
+            if self.main_stack.get_visible_child_name() == "app_page":
+                self.switchbar_bottom.set_reveal(True)
+            self.player_timebar.set_visible(False)
+            self.play_songinfo.set_hexpand(True)
+        else:
+            self.header_stack.set_visible_child_name("switchbar_header")
+            self.switchbar_bottom.set_reveal(False)
+            self.player_timebar.set_visible(True)
+            self.play_songinfo.set_hexpand(False)
+
+    def resize_player(self, *_):
+        if self.main_stack.get_visible_child_name() == "app_page":
+            self.main_stack.set_visible_child_name("player_page")
+            self.switchbar_bottom.set_reveal(False)
+            self.player_reveal.set_reveal_child(False)
+        elif self.main_stack.get_visible_child_name() == "player_page":
+            self.main_stack.set_visible_child_name("app_page")
+            self.switchbar_bottom.set_reveal(True)
+            self.player_reveal.set_reveal_child(True)
+
+            
