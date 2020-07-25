3d2800b3d71abec091c0ba38ae25119488390bcb
Aurnytoraink <wiiu3dsxl13127@gmail.com>
2020-07-25 23:40:24 +0200

    Redesigned the interface to use libhandy

--- src/tidalgtk/window.py
+++ src/tidalgtk/window.py
@@ -19,47 +19,41 @@ from gi.repository import Gtk, Handy
 # from tidalgtk.api.session import Session
 
 @Gtk.Template(resource_path='/com/github/Aurnytoraink/TidalGTK/ui/window.ui')
-class TidalgtkWindow(Gtk.ApplicationWindow):
+class TidalgtkWindow(Handy.ApplicationWindow):
     __gtype_name__ = 'TidalgtkWindow'
 
-    header_stack = Gtk.Template.Child()
-    main_stack = Gtk.Template.Child()
     app_stack = Gtk.Template.Child()
-    player_reveal = Gtk.Template.Child()
-    switchbar_bottom = Gtk.Template.Child()
     switchbar_bottom = Gtk.Template.Child()
     player_timebar = Gtk.Template.Child()
     play_songinfo = Gtk.Template.Child()
     enlarge_player_button = Gtk.Template.Child()
-    reduce_player_button = Gtk.Template.Child()
+    close_player_button = Gtk.Template.Child()
+    deck_app = Gtk.Template.Child()
+    header_switch = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.connect("check-resize",self.update_scale_interface)
-        self.enlarge_player_button.connect("clicked",self.resize_player)
-        self.reduce_player_button.connect("clicked",self.resize_player)
+        self.enlarge_player_button.connect("clicked",self.display_player)
+        self.close_player_button.connect("clicked",self.display_player)
 
     def update_scale_interface(self, *_):
-        width, height = self.get_size()
-        if width <= 545:
-            self.header_stack.set_visible_child_name("title_header")
-            if self.main_stack.get_visible_child_name() == "app_page":
-                self.switchbar_bottom.set_reveal(True)
+        if self.header_switch.get_title_visible():
+            self.switchbar_bottom.set_reveal(True)
             self.player_timebar.set_visible(False)
             self.play_songinfo.set_hexpand(True)
         else:
-            self.header_stack.set_visible_child_name("switchbar_header")
             self.switchbar_bottom.set_reveal(False)
             self.player_timebar.set_visible(True)
             self.play_songinfo.set_hexpand(False)
 
-    def resize_player(self, *_):
-        if self.main_stack.get_visible_child_name() == "app_page":
-            self.main_stack.set_visible_child_name("player_page")
+    def display_player(self, *_):
+        if self.deck_app.get_visible_child_name() == "app_page":
+            self.deck_app.set_visible_child_name("player_page")
             self.switchbar_bottom.set_reveal(False)
             self.player_reveal.set_reveal_child(False)
-        elif self.main_stack.get_visible_child_name() == "player_page":
-            self.main_stack.set_visible_child_name("app_page")
+        elif self.deck_app.get_visible_child_name() == "player_page":
+            self.deck_app.set_visible_child_name("app_page")
             self.switchbar_bottom.set_reveal(True)
             self.player_reveal.set_reveal_child(True)
 
