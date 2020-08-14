b7e7cfffdbe9899bff3e11f2e83b296402ce46cd
Aurnytoraink <wiiu3dsxl13127@gmail.com>
2020-08-14 14:43:43 +0200

    Add licence to models.py file

--- src/tidalgtk/models.py
+++ src/tidalgtk/models.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 class Track():
 	def __init__(self, uri, title, artist, duration, cover, like=False):
 		self.uri = uri
