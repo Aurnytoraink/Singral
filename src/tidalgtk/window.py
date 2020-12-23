12176f931b3e1af77d64a55bc607c38414b1205e
Aurnytoraink <wiiu3dsxl13127@gmail.com>
2020-12-23 22:57:58 +0100

    WIP: Redesign app page

--- src/tidalgtk/window.py
+++ src/tidalgtk/window.py
@@ -59,13 +59,6 @@ class TidalgtkWindow(Handy.ApplicationWindow):
     playlist_flowbox = Gtk.Template.Child()
     playlist_box = Gtk.Template.Child()
 
-    #Favorites Page
-    fav_listbox = Gtk.Template.Child()
-    fav_stack = Gtk.Template.Child()
-    fav_albums_flowbox = Gtk.Template.Child()
-    fav_artists_flowbox = Gtk.Template.Child()
-    fav_tracks_listbox = Gtk.Template.Child()
-
     #Player UI
     duration_scale = Gtk.Template.Child()
     enlarge_player_button = Gtk.Template.Child()
@@ -175,31 +168,9 @@ class TidalgtkWindow(Handy.ApplicationWindow):
     def logoff(self,*_):
         self.session.logoff()
         self.main_stack.set_visible_child_name("login_page")
-        for child in self.fav_albums_flowbox.get_children():
-            self.fav_albums_flowbox.remove(child)
-        for child in self.fav_artists_flowbox.get_children():
-            self.fav_artists_flowbox.remove(child)
-        for child in self.fav_tracks_flowbox.get_children():
-            self.fav_tracks_flowbox.remove(child)
-        for child in self.fav_tracks_listbox.get_children():
-            self.fav_tracks_listbox.remove(child)
 
     def create_account(self,*_):
         Gtk.show_uri_on_window(self,"https://www.qobuz.com",Gdk.CURRENT_TIME)
 
     def forget_pwd(self,*_):
         Gtk.show_uri_on_window(self,"https://www.qobuz.com/reset-password",Gdk.CURRENT_TIME)
-
-    # Library Page #
-
-    def get_fav_albums(self,*_):
-        threading.Thread(target=self.session.get_userfav_albums).start()
-
-    def get_fav_artists(self,*_):
-        threading.Thread(target=self.session.get_userfav_artists).start()
-
-    def get_fav_tracks(self,*_):
-        threading.Thread(target=self.session.get_userfav_tracks).start()
-
-    def get_fav_playlists(self,*_):
-        threading.Thread(target=self.session.get_userfav_playlists).start()
